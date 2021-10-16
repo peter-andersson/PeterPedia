b31b7db57ae46c6cfd50ea41b2869633a77eb106
Peter Andersson <peter@lpda.se>
2021-10-16 12:46:19 +0200

    Client for bookmarks and reading list

---
+++ src/Client.Bookmark/Pages/ReadLater.razor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PeterPedia.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace PeterPedia.Client.Bookmark.Pages
+{
+    public partial class ReadLater : ComponentBase
+    {
+        public List<ReadListItem> Items { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            // TODO: Load data...
+            Items = new List<ReadListItem>();
+
+            Items.Add(new ReadListItem()
+            {
+                Id = 1,
+                Url = "https://norran.se",
+                Added = DateTime.Now
+            });
+
+            Items.Add(new ReadListItem()
+            {
+                Id = 2,
+                Url = "https://aftonbladet.se",
+                Added = DateTime.Now
+            });
+        }
+    }
+}
---
+++ src/Client.Bookmark/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './**/*.html',
+      './**/*.razor'
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
