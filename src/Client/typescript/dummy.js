b1303b3ebed3bceaf688c7d1cb1950e1c9a8c4ce
Peter Andersson <peter@lpda.se>
2022-02-26 11:05:40 +0100

    Added to remove errors in Visual Studio

---
+++ src/Client/typescript/dummy.js
@@ -0,0 +1,2 @@
+"use strict";
+// Dummy files just to make errors in Visual Studio go away. Probably should be handled in a better way.
---
+++ src/Client/typescript/dummy.ts
@@ -0,0 +1,1 @@
+﻿// Dummy files just to make errors in Visual Studio go away. Probably should be handled in a better way.
