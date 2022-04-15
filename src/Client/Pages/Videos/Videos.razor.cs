343436657c41e1e8aa5d190960de489ba54aec00
Peter Andersson <peter@lpda.se>
2022-04-15 09:34:43 +0200

    Removed table on videos page
    Possible to sort by duration and title

--- src/Client/Pages/Videos/Videos.razor.cs
+++ src/Client/Pages/Videos/Videos.razor.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components;
 
 namespace PeterPedia.Client.Pages.Videos;
 
@@ -7,17 +7,42 @@ public partial class Videos : ComponentBase
     [Inject]
     public VideoService VideoService { get; set; } = null!;
 
-    public List<Video> VideoList { get; set; } = null!;
+    private readonly List<Video> _videos = new();
+
+    public List<Video> VideoList
+    {
+        get
+        {
+            IEnumerable<Video> query = _videos;
+
+            if (!string.IsNullOrWhiteSpace(Filter))
+            {
+                query = query.Where(v => v.Title.Contains(Filter, StringComparison.InvariantCultureIgnoreCase));
+            }
+
+            query = SortDescending
+                ? OrderByDuration ? query.OrderByDescending(v => v.Duration) : (IEnumerable<Video>)query.OrderByDescending(v => v.Title)
+                : OrderByDuration ? query.OrderBy(v => v.Duration) : (IEnumerable<Video>)query.OrderBy(v => v.Title);
+
+            return query.ToList();
+        }
+    }
+
+    public string Filter { get; set; } = string.Empty;
+
+    public bool OrderByDuration { get; set; }
+
+    public bool SortDescending { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
         await VideoService.FetchDataAsync();
 
-        FilterVideos(string.Empty);
+        _videos.Clear();
+        _videos.AddRange(VideoService.Videos);             
     }
 
-    public void FilterVideos(string filter)
-    {
-        VideoList = VideoService.Videos.Where(v => v.Title.Contains(filter, StringComparison.InvariantCultureIgnoreCase)).ToList();
-    }
+    public void ToggleOrder() => OrderByDuration = !OrderByDuration;
+
+    public void ToggleDescending() => SortDescending = !SortDescending;
 }
