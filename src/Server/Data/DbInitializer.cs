cbc2bd0761fa7035a0ae11ade338f5715a014ab0
Peter Andersson <peter@lpda.se>
2022-04-24 10:56:22 +0200

    Fixed wrong data in LastUpdated for books.
    Fixes #37

--- src/Server/Data/DbInitializer.cs
+++ src/Server/Data/DbInitializer.cs
@@ -1,18 +1,19 @@
-﻿using System;
 using Microsoft.EntityFrameworkCore;
 
-namespace PeterPedia.Server.Data
-{
-    public static class DbInitializer
-    {
-        public static void Initialize(PeterPediaContext context)
-        {
-            if (context is null)
-            {
-                throw new ArgumentNullException(nameof(context));
-            }
+namespace PeterPedia.Server.Data;
 
-            context.Database.Migrate();
+public static class DbInitializer
+{
+    public static void Initialize(PeterPediaContext context)
+    {
+        if (context is null)
+        {
+            throw new ArgumentNullException(nameof(context));
         }
+
+        context.Database.Migrate();
+
+        var command = "UPDATE book SET LastUpdated = '2000-01-01' WHERE LastUpdated = 'LastUpdated'";            
+        context.Database.ExecuteSqlRaw(command);
     }
 }
