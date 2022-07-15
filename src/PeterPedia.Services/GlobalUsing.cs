f1361c21f657e5526bef8bd57b0951430a910a63
Peter Andersson <peter@lpda.se>
2022-07-15 15:40:35 +0200

    Removed services no longer needed

--- src/PeterPedia.Services/GlobalUsing.cs
+++ src/PeterPedia.Services/GlobalUsing.cs
@@ -3,5 +3,4 @@ global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.Logging;
 global using PeterPedia.Data;
 global using PeterPedia.Data.Models;
-global using PeterPedia.Services.LogMessages;
 global using PeterPedia.Services.Models;
