f5395e9823ddc50c66507cd2f7d2f079a17d1e56
Peter Andersson <peter@lpda.se>
2022-07-16 11:33:18 +0200

    Added entity for subscriptions and articles.
    Removed old entity framework code.

---
+++ src/PeterPedia.Data/Models/ArticleEntity.cs
@@ -0,0 +1,28 @@
+using Newtonsoft.Json;
+using PeterPedia.Data.Interface;
+
+namespace PeterPedia.Data.Models;
+
+public class ArticleEntity : IEntity
+{
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; } = string.Empty;
+
+    public string PartitionKey => Id;
+
+#pragma warning disable CA1822 // Mark members as static
+    public string Type => "article";
+#pragma warning restore CA1822 // Mark members as static
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Url { get; set; } = string.Empty;
+
+    public string Group { get; set; } = string.Empty;
+
+    public string Subscription { get; set; } = string.Empty;
+
+    public DateTime PublishDate { get; set; }
+
+    public DateTime? ReadDate { get; set; }
+}
---
+++ src/PeterPedia.Data/Models/SubscriptionEntity.cs
@@ -0,0 +1,28 @@
+using Newtonsoft.Json;
+using PeterPedia.Data.Interface;
+
+namespace PeterPedia.Data.Models;
+
+public class SubscriptionEntity : IEntity
+{
+    [JsonProperty(PropertyName = "id")]
+    public string Id { get; set; } = string.Empty;
+
+    public string PartitionKey => Id;
+
+#pragma warning disable CA1822 // Mark members as static
+    public string Type => "subscription";
+#pragma warning restore CA1822 // Mark members as static
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Url { get; set; } = string.Empty;
+
+    public int UpdateIntervalMinute { get; set; }
+
+    public string? Group { get; set; }
+
+    public DateTime NextUpdate { get; set; }
+
+    public string? Hash { get; set; }
+}
