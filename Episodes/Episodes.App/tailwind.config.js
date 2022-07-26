efbed0f35f6d5584c7e5e37882b1d223cc70b644
Peter Andersson <peter@lpda.se>
2022-07-26 10:42:12 +0200

    Added tailwindCSS

---
+++ Episodes/Episodes.App/tailwind.config.js
@@ -0,0 +1,10 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,razor}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
